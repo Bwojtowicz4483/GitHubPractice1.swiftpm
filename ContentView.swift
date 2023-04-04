a59701c86ab0a49f37aa10fd596ef3aaee812c58
Pavel Sliziuk <psliziuk6381@stu.d214.org>
2023-04-04 14:45:49 -0500

    enjoy

--- ContentView.swift
+++ ContentView.swift
@@ -3,10 +3,12 @@ import SwiftUI
 struct ContentView: View {
     @State var Number1 = 0
     @State var Number2 = 0
+    @State var answer = 0
     var body: some View {
         HStack {
             TextField("1", value: $Number1, format: .number)
             TextField("2", value: $Number2, format: .number)
+            Button(action: <#T##() -> Void#>, label: "Multiply")
         }
     }
 }
